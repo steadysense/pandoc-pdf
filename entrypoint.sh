47959f8a8a60e0de09511c24da124536ebd976a4
vspath <31615284+vspath@users.noreply.github.com>
2021-07-12 15:58:31 +0200

    Changed loop in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ echo "Github workspace files:"
 ls -la "/github/workspace"
 
 echo "Template directory file:"
-ls -la "templates"
+ls -la $1
 
 echo "Documents to convert:"
-ls -la "documents"
+ls -la $2
 
-for f in $(find "/documents" -name "*.md" | grep "FB\|PB\|DA"); do
+for f in $(find "$2" -name "*.md" | grep "FB\|PB\|DA"); do
   echo "Converting $f"
   build.sh $f
 done
