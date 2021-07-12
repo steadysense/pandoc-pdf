ca1918604d9ef80dee6076edad24eb67e654338b
vspath <31615284+vspath@users.noreply.github.com>
2021-07-12 11:34:23 +0200

    Changed document filepath

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-for f in $(find $2 -name "*.md" | grep "FB\|PB\|DA"); do
+for f in $(find "/data/documents" -name "*.md" | grep "FB\|PB\|DA"); do
   echo "Converting $f"
   build.sh $f
 done
