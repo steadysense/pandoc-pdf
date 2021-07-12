d3319bc8b50b414f9c473937c5401e2f87d3d0fb
vspath <31615284+vspath@users.noreply.github.com>
2021-07-12 10:54:50 +0200

    Removed comments in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -euf -o pipefail
 
-echo "Template files"
-echo "Changing pwd to $1"
-cd "$(dirname "$1")"
-
-echo "Directory of files to convert"
-echo "$2"
-
 for f in $(find $2 -name "*.md" | grep "FB\|PB\|DA"); do
   build.sh $f
 done
