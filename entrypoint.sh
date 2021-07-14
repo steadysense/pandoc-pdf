f78d6b50972db46fb3cab5df9160bba9d862d7dd
vspath <31615284+vspath@users.noreply.github.com>
2021-07-14 12:07:02 +0200

    Added input paths to gh-workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,13 @@ echo "Github workspace files:"
 ls -la "/github/workspace"
 
 echo "Template directory file:"
-ls -la "/github/workspace/$1"
+#ls -la "/github/workspace/$1"
+ls -la "/github/workspace/templates"
 
 echo "Documents to convert:"
-ls -la "/github/workspace/$2"
+#ls -la "/github/workspace/$2"
+ls -la "/github/workspace/documents"
+
 
 for f in $(find "/github/workspace/$2" -name "*.md" | grep "FB\|PB\|DA"); do
   echo "Converting $f"
