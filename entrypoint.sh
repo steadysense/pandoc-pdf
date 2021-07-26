b438ebf3794f9efca006628e98e70ebe5b0467b0
Moritz Federspiel <moritz.federspiel@steadysense.at>
2021-07-26 17:26:57 +0200

    Remove - from variable names, fix build.sh again, use vars to cd into templates and build globfor documents

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,13 @@ set -euf -o pipefail
 echo "$(pwd)"
 echo "$(ls -la)"
 
-exec "$@"
+echo "$(env)"
+
+echo "Document Directory: $INPUT_DOCUMENT_DIRECTORY"
+echo "Template Directory: $INPUT_TEMPLATE_DIRECTORY"
+
+let DOCUMENT_GLOB = "$(pwd)/$INPUT_DOCUMENT_DIRECTORY/**/*.md"
+
+cd "${INPUT_TEMPLATE_DIRECTORY}"
+exec "$@" "${DOCUMENT_GLOB}"
 
